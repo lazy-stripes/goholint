5c642ae4ae4ab110850e9a3f37fdeb641819ce0d
kefen <lazy-stripes@tigris.fr>
2018-08-02 23:28:07 +0200

    Cleanup in ROM.

--- memory/rom.go
+++ memory/rom.go
@@ -7,7 +7,7 @@ import (
 
 // ROM is a read-only special case of RAM, initialized from a binary file.
 type ROM struct {
-	*RAM
+	RAM
 }
 
 // NewROM instantiates a read-only chunk of memory from a binary dump.
@@ -17,7 +17,7 @@ func NewROM(filename string, start uint) *ROM {
 		fmt.Printf(" !!! Cannot read ROM file %s (%s)\n", filename, err)
 		return nil
 	}
-	return &ROM{RAM: &RAM{Start: start, Bytes: bytes}}
+	return &ROM{RAM{Start: start, Bytes: bytes}}
 }
 
 // Write does nothing and displays an error, for reasons I hope are obvious.
