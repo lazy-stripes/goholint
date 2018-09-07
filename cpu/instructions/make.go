404b419b17210190a8b3a694e6b45ad3e209417d
kefen <lazy-stripes@tigris.fr>
2018-09-07 18:21:48 +0200

    Templates work. Using simplistic struct names.
    Porting old instructions, implementing new ones, living the dream.

--- cpu/instructions/make.go
+++ cpu/instructions/make.go
@@ -18,10 +18,46 @@ type data struct {
 	Operator    string
 }
 
-func main() {
+// typeName generates a deterministic name for derived instruction types.
+// TODO: I'm going the easy way for now, but I'd love being able to generate
+//		 human-readable name like nop, ldDeD16, etc.
+func typeName(d *data) (name string) {
+	return fmt.Sprintf("op%s", strings.Title(fmt.Sprintf("%02x", d.Opcode)))
+}
 
+func main() {
 	instructions := []data{
 		{Opcode: 0x00, Template: "nop"},
+		{Opcode: 0x01, Template: "ldrrd16", High: "B", Low: "C"},
+		{Opcode: 0x02, Template: "ldaddrr", Address: "BC", Register: "A"},
+		{Opcode: 0x03, Template: "incrr", High: "B", Low: "C"},
+		{Opcode: 0x04, Template: "incr", Register: "B"},
+		{Opcode: 0x05, Template: "decr", Register: "B"},
+		{Opcode: 0x06, Template: "ldrd8", Register: "B"},
+		{Opcode: 0x07, Template: "rlca", Register: "A"},
+
+		{Opcode: 0x0c, Template: "incr", Register: "C"},
+		{Opcode: 0x0d, Template: "decr", Register: "C"},
+		{Opcode: 0x0e, Template: "ldrd8", Register: "C"},
+		{Opcode: 0x11, Template: "ldrrd16", High: "D", Low: "E"},
+		{Opcode: 0x13, Template: "incrr", High: "D", Low: "E"},
+		{Opcode: 0x14, Template: "incr", Register: "D"},
+		{Opcode: 0x15, Template: "decr", Register: "D"},
+		{Opcode: 0x16, Template: "ldrd8", Register: "D"},
+		{Opcode: 0x1c, Template: "incr", Register: "E"},
+		{Opcode: 0x1d, Template: "decr", Register: "E"},
+		{Opcode: 0x1e, Template: "ldrd8", Register: "E"},
+		{Opcode: 0x21, Template: "ldrrd16", High: "H", Low: "L"},
+		{Opcode: 0x23, Template: "incrr", High: "H", Low: "L"},
+		{Opcode: 0x24, Template: "incr", Register: "H"},
+		{Opcode: 0x25, Template: "decr", Register: "H"},
+		{Opcode: 0x26, Template: "ldrd8", Register: "H"},
+		{Opcode: 0x2c, Template: "incr", Register: "L"},
+		{Opcode: 0x2d, Template: "decr", Register: "L"},
+		{Opcode: 0x2e, Template: "ldrd8", Register: "L"},
+		{Opcode: 0x3c, Template: "incr", Register: "A"},
+		{Opcode: 0x3d, Template: "decr", Register: "A"},
+		{Opcode: 0x3e, Template: "ldrd8", Register: "A"},
 		{Opcode: 0xa0, Template: "boolreg", Instruction: "AND", Operator: "&=", Register: "B"},
 		{Opcode: 0xa1, Template: "boolreg", Instruction: "AND", Operator: "&=", Register: "C"},
 		{Opcode: 0xa2, Template: "boolreg", Instruction: "AND", Operator: "&=", Register: "D"},
@@ -50,9 +86,9 @@ func main() {
 		// 0xb7: orA,
 	}
 
-	funcs := template.FuncMap{"lower": strings.ToLower}
-	t := template.New("header.tmpl").Funcs(funcs)
-	t = template.Must(t.ParseGlob("instructions/*.tmpl"))
+	funcs := template.FuncMap{"lower": strings.ToLower, "name": typeName}
+	t := template.New("header.gotmpl").Funcs(funcs)
+	t = template.Must(t.ParseGlob("instructions/*.gotmpl"))
 
 	// Render all instructions in opcode order.
 	if f, err := os.Create("instructionset.go"); err == nil {
@@ -60,10 +96,14 @@ func main() {
 			f.Close()
 		}()
 
-		t.Execute(f, nil)
+		// Header and instruction set array.
+		t.Execute(f, instructions)
 
 		for _, i := range instructions {
-			t.ExecuteTemplate(f, fmt.Sprintf("%s.tmpl", i.Template), i)
+			err = t.ExecuteTemplate(f, fmt.Sprintf("%s.gotmpl", i.Template), &i)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 }
--- cpu/instructionset.go
+++ cpu/instructionset.go
@@ -1,21 +1,516 @@
 // Generated code. See instructions.go
 package cpu
 
+// LR35902InstructionSet is an array of instrutions for the DMG CPU.
+var LR35902InstructionSet = [...]Instruction{
+	0x00: op00{},
+	0x01: op01{},
+	0x02: op02{},
+	0x03: op03{},
+	0x04: op04{},
+	0x05: op05{},
+	0x06: op06{},
+	0x07: op07{},
+	0x0c: op0c{},
+	0x0d: op0d{},
+	0x0e: op0e{},
+	0x11: op11{},
+	0x13: op13{},
+	0x14: op14{},
+	0x15: op15{},
+	0x16: op16{},
+	0x1c: op1c{},
+	0x1d: op1d{},
+	0x1e: op1e{},
+	0x21: op21{},
+	0x23: op23{},
+	0x24: op24{},
+	0x25: op25{},
+	0x26: op26{},
+	0x2c: op2c{},
+	0x2d: op2d{},
+	0x2e: op2e{},
+	0x3c: op3c{},
+	0x3d: op3d{},
+	0x3e: op3e{},
+	0xa0: opA0{},
+	0xa1: opA1{},
+	0xa2: opA2{},
+	0xa3: opA3{},
+	0xa4: opA4{},
+	0xa5: opA5{},
+	0xa6: opA6{},
+}
+
+// LR35902ExtendedInstructionSet is the array of extension opcodes for the DMG CPU.
+var LR35902ExtendedInstructionSet = []Instruction{}
+
 // 00: NOP			4 cycles
-type nop struct {
+type op00 struct {
 	SingleStepOp
 }
 
-func (op nop) Execute(c *CPU) (done bool) {
+func (op op00) Execute(c *CPU) (done bool) {
+	return true
+}
+
+// 01: LD BC,d16		(12 cycles)
+type op01 struct {
+	MultiStepsOp
+}
+
+func (op op01) Tick() (done bool) {
+	switch op.step {
+	case 0:
+		op.cpu.C = op.cpu.NextByte()
+		op.step++
+	case 1:
+		op.cpu.B = op.cpu.NextByte()
+		done = true
+	}
+	return
+}
+
+// 02: LD (BC),A		(8 cycles)
+type op02 struct {
+	MultiStepsOp
+}
+
+func (op op02) Tick() (done bool) {
+	op.cpu.MMU.Write(uint(op.cpu.BC()), op.cpu.A)
+	return true
+}
+
+// 03: INC BC			8 cycles
+type op03 struct {
+	MultiStepsOp
+}
+
+func (op op03) Tick() (done bool) {
+	if op.cpu.C == 0xff {
+		op.cpu.B++
+	}
+	op.cpu.C++
+	return true
+}
+
+// 04: INC B			4 cycles
+type op04 struct {
+	SingleStepOp
+}
+
+func (op op04) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.B > 0x0F {
+		c.F |= FlagH
+	}
+	c.B++
+	if c.B == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 05: DEC B			4 cycles
+type op05 struct {
+	SingleStepOp
+}
+
+func (op op05) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.B > 0x0F {
+		c.F |= FlagH
+	}
+	c.B--
+	if c.B == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 06: LD B,d8		(8 cycles)
+type op06 struct {
+	MultiStepsOp
+}
+
+func (op op06) Tick() (done bool) {
+	op.cpu.B = op.cpu.NextByte()
+	return true
+}
+
+// 07: RLCA			4 cycles
+type op07 struct {
+	SingleStepOp
+}
+
+func (op op07) Execute(c *CPU) (done bool) {
+	// FIXME: [OPCODES] says flags are 0 0 0 c. Couldn't confirm.
+	// Flags z 0 0 c
+	c.F = 0x00
+	result := c.A << 1 & 0xff
+	if c.A&0x80 != 0 {
+		result |= 1
+		c.F |= FlagC
+	}
+
+	if result == 0 {
+		c.F |= FlagZ
+	}
+	c.A = result
+	return true
+}
+
+// 0C: INC C			4 cycles
+type op0c struct {
+	SingleStepOp
+}
+
+func (op op0c) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.C > 0x0F {
+		c.F |= FlagH
+	}
+	c.C++
+	if c.C == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 0D: DEC C			4 cycles
+type op0d struct {
+	SingleStepOp
+}
+
+func (op op0d) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.C > 0x0F {
+		c.F |= FlagH
+	}
+	c.C--
+	if c.C == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 0E: LD C,d8		(8 cycles)
+type op0e struct {
+	MultiStepsOp
+}
+
+func (op op0e) Tick() (done bool) {
+	op.cpu.C = op.cpu.NextByte()
+	return true
+}
+
+// 11: LD DE,d16		(12 cycles)
+type op11 struct {
+	MultiStepsOp
+}
+
+func (op op11) Tick() (done bool) {
+	switch op.step {
+	case 0:
+		op.cpu.E = op.cpu.NextByte()
+		op.step++
+	case 1:
+		op.cpu.D = op.cpu.NextByte()
+		done = true
+	}
+	return
+}
+
+// 13: INC DE			8 cycles
+type op13 struct {
+	MultiStepsOp
+}
+
+func (op op13) Tick() (done bool) {
+	if op.cpu.E == 0xff {
+		op.cpu.D++
+	}
+	op.cpu.E++
+	return true
+}
+
+// 14: INC D			4 cycles
+type op14 struct {
+	SingleStepOp
+}
+
+func (op op14) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.D > 0x0F {
+		c.F |= FlagH
+	}
+	c.D++
+	if c.D == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 15: DEC D			4 cycles
+type op15 struct {
+	SingleStepOp
+}
+
+func (op op15) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.D > 0x0F {
+		c.F |= FlagH
+	}
+	c.D--
+	if c.D == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 16: LD D,d8		(8 cycles)
+type op16 struct {
+	MultiStepsOp
+}
+
+func (op op16) Tick() (done bool) {
+	op.cpu.D = op.cpu.NextByte()
+	return true
+}
+
+// 1C: INC E			4 cycles
+type op1c struct {
+	SingleStepOp
+}
+
+func (op op1c) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.E > 0x0F {
+		c.F |= FlagH
+	}
+	c.E++
+	if c.E == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 1D: DEC E			4 cycles
+type op1d struct {
+	SingleStepOp
+}
+
+func (op op1d) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.E > 0x0F {
+		c.F |= FlagH
+	}
+	c.E--
+	if c.E == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 1E: LD E,d8		(8 cycles)
+type op1e struct {
+	MultiStepsOp
+}
+
+func (op op1e) Tick() (done bool) {
+	op.cpu.E = op.cpu.NextByte()
+	return true
+}
+
+// 21: LD HL,d16		(12 cycles)
+type op21 struct {
+	MultiStepsOp
+}
+
+func (op op21) Tick() (done bool) {
+	switch op.step {
+	case 0:
+		op.cpu.L = op.cpu.NextByte()
+		op.step++
+	case 1:
+		op.cpu.H = op.cpu.NextByte()
+		done = true
+	}
+	return
+}
+
+// 23: INC HL			8 cycles
+type op23 struct {
+	MultiStepsOp
+}
+
+func (op op23) Tick() (done bool) {
+	if op.cpu.L == 0xff {
+		op.cpu.H++
+	}
+	op.cpu.L++
+	return true
+}
+
+// 24: INC H			4 cycles
+type op24 struct {
+	SingleStepOp
+}
+
+func (op op24) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.H > 0x0F {
+		c.F |= FlagH
+	}
+	c.H++
+	if c.H == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 25: DEC H			4 cycles
+type op25 struct {
+	SingleStepOp
+}
+
+func (op op25) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.H > 0x0F {
+		c.F |= FlagH
+	}
+	c.H--
+	if c.H == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 26: LD H,d8		(8 cycles)
+type op26 struct {
+	MultiStepsOp
+}
+
+func (op op26) Tick() (done bool) {
+	op.cpu.H = op.cpu.NextByte()
+	return true
+}
+
+// 2C: INC L			4 cycles
+type op2c struct {
+	SingleStepOp
+}
+
+func (op op2c) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.L > 0x0F {
+		c.F |= FlagH
+	}
+	c.L++
+	if c.L == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 2D: DEC L			4 cycles
+type op2d struct {
+	SingleStepOp
+}
+
+func (op op2d) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.L > 0x0F {
+		c.F |= FlagH
+	}
+	c.L--
+	if c.L == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 2E: LD L,d8		(8 cycles)
+type op2e struct {
+	MultiStepsOp
+}
+
+func (op op2e) Tick() (done bool) {
+	op.cpu.L = op.cpu.NextByte()
+	return true
+}
+
+// 3C: INC A			4 cycles
+type op3c struct {
+	SingleStepOp
+}
+
+func (op op3c) Execute(c *CPU) (done bool) {
+	// Flags z 0 h -
+	c.F &= ^FlagN
+	if c.A > 0x0F {
+		c.F |= FlagH
+	}
+	c.A++
+	if c.A == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 3D: DEC A			4 cycles
+type op3d struct {
+	SingleStepOp
+}
+
+func (op op3d) Execute(c *CPU) (done bool) {
+	// Flags z 1 h -
+	c.F &= FlagC
+	c.F |= FlagN
+	if c.A > 0x0F {
+		c.F |= FlagH
+	}
+	c.A--
+	if c.A == 0 {
+		c.F |= FlagZ
+	}
+	return true
+}
+
+// 3E: LD A,d8		(8 cycles)
+type op3e struct {
+	MultiStepsOp
+}
+
+func (op op3e) Tick() (done bool) {
+	op.cpu.A = op.cpu.NextByte()
 	return true
 }
 
 // A0: AND B
-type andB struct {
+type opA0 struct {
 	SingleStepOp
 }
 
-func (op andB) Execute(c *CPU) (done bool) {
+func (op opA0) Execute(c *CPU) (done bool) {
 	c.A &= c.B
 	// Flags z 0 0 0
 	if c.A == 0 {
@@ -27,11 +522,11 @@ func (op andB) Execute(c *CPU) (done bool) {
 }
 
 // A1: AND C
-type andC struct {
+type opA1 struct {
 	SingleStepOp
 }
 
-func (op andC) Execute(c *CPU) (done bool) {
+func (op opA1) Execute(c *CPU) (done bool) {
 	c.A &= c.C
 	// Flags z 0 0 0
 	if c.A == 0 {
@@ -43,11 +538,11 @@ func (op andC) Execute(c *CPU) (done bool) {
 }
 
 // A2: AND D
-type andD struct {
+type opA2 struct {
 	SingleStepOp
 }
 
-func (op andD) Execute(c *CPU) (done bool) {
+func (op opA2) Execute(c *CPU) (done bool) {
 	c.A &= c.D
 	// Flags z 0 0 0
 	if c.A == 0 {
@@ -59,11 +554,11 @@ func (op andD) Execute(c *CPU) (done bool) {
 }
 
 // A3: AND E
-type andE struct {
+type opA3 struct {
 	SingleStepOp
 }
 
-func (op andE) Execute(c *CPU) (done bool) {
+func (op opA3) Execute(c *CPU) (done bool) {
 	c.A &= c.E
 	// Flags z 0 0 0
 	if c.A == 0 {
@@ -75,11 +570,11 @@ func (op andE) Execute(c *CPU) (done bool) {
 }
 
 // A4: AND H
-type andH struct {
+type opA4 struct {
 	SingleStepOp
 }
 
-func (op andH) Execute(c *CPU) (done bool) {
+func (op opA4) Execute(c *CPU) (done bool) {
 	c.A &= c.H
 	// Flags z 0 0 0
 	if c.A == 0 {
@@ -91,11 +586,11 @@ func (op andH) Execute(c *CPU) (done bool) {
 }
 
 // A5: AND L
-type andL struct {
+type opA5 struct {
 	SingleStepOp
 }
 
-func (op andL) Execute(c *CPU) (done bool) {
+func (op opA5) Execute(c *CPU) (done bool) {
 	c.A &= c.L
 	// Flags z 0 0 0
 	if c.A == 0 {
@@ -107,11 +602,11 @@ func (op andL) Execute(c *CPU) (done bool) {
 }
 
 // A6: AND (HL)
-type andAddrHL struct {
+type opA6 struct {
 	MultiStepsOp
 }
 
-func (op andAddrHL) Tick() (done bool) {
+func (op opA6) Tick() (done bool) {
 	op.cpu.A &= op.cpu.MMU.Read(uint(op.cpu.HL()))
 	// Flags z 0 0 0
 	if op.cpu.A == 0 {
