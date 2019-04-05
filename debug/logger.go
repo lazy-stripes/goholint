6fa4a57957b1898a0f0553e4b03e937e09ee2369
kefen <lazy-stripes@tigris.fr>
2019-04-05 18:02:00 +0200

    Add message repetition detection to debug logger.

--- debug/logger.go
+++ debug/logger.go
@@ -6,6 +6,13 @@ import (
 
 // Name-based on/off logger for simple debugging purposes.
 
+// Copy of the last string displayed to gracefully handle repeated text.
+// TODO: making this into a FIFO with a configurable size would be awesome.
+var lastMessage string
+
+// Count of how many times the last message was repeated.
+var lastMessageCount uint
+
 // Enabled setting controls whether logging will occur for a given module name
 // when the usual methods are called. Default is no logging. Enabling 'all'
 // will turn on debug output for every module.
@@ -22,7 +29,18 @@ var Context = func() string { return "" }
 func Print(name string, v ...interface{}) {
 	if Enabled[name] || Enabled["all"] {
 		fmt.Print(Context())
-		fmt.Println(fmt.Sprint(v...))
+		msg := fmt.Sprint(v...)
+		if msg == lastMessage {
+			lastMessageCount++
+			fmt.Printf(" ... repeated %d times\r", lastMessageCount)
+		} else {
+			if lastMessageCount > 1 {
+				fmt.Println()
+			}
+			lastMessage = msg
+			lastMessageCount = 1
+			fmt.Println(msg)
+		}
 	}
 }
 
@@ -30,17 +48,11 @@ func Print(name string, v ...interface{}) {
 // for the given module name (or if 'all' was enabled). Also it automatically
 // tacks a linefeed at the end.
 func Printf(name string, format string, v ...interface{}) {
-	if Enabled[name] || Enabled["all"] {
-		fmt.Print(Context())
-		fmt.Println(fmt.Sprintf(format, v...))
-	}
+	Print(name, fmt.Sprintf(format, v...))
 }
 
 // Println displays a message just like fmt.Println but only if Enabled is true
 // for the given module name (or if 'all' was enabled).
 func Println(name string, v ...interface{}) {
-	if Enabled[name] || Enabled["all"] {
-		fmt.Print(Context())
-		fmt.Println(v...)
-	}
+	Print(name, v...)
 }
