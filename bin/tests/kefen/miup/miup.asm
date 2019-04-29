89f7a5e06a387d0895319dfb38fd98f096f8f6d9
kefen <lazy-stripes@tigris.fr>
2019-04-30 00:58:28 +0200

    MIUP demo ROM v0.0.1. Damn assembly is fun, but tedious!

--- bin/tests/kefen/miup/miup.asm
+++ bin/tests/kefen/miup/miup.asm
@@ -4,12 +4,19 @@
 
 INCLUDE "header.asm"
 
+; Constants.
+DELAY_ADDR EQU $D000
+NEXT_STATE_ADDR EQU $D001	; $D001-$D002
+WORK_OAM_ADDR EQU $C000
+
+FADE_DELAY EQU $20
+
 SECTION "default", ROM0
 main:
 init_oam:
 	; Initialize shadow OAM
 	LD DE, sprites_data
-	LD HL, $C000
+	LD HL, WORK_OAM_ADDR
 	LD C, sprites_data.end - sprites_data
 .copy_loop
 	LD A, [DE]
@@ -18,12 +25,10 @@ init_oam:
 	DEC C
 	JR NZ, .copy_loop
 
-FADE_DELAY_ADDR EQU $D000
-FADE_DELAY EQU $10
 init_fade:
 	; Set delay between each color change (in frame number).
 	LD A, FADE_DELAY
-	LD HL, FADE_DELAY_ADDR
+	LD HL, DELAY_ADDR
 	LD [HL], A
 
 	; Logo doesn't care about the palette's high nibble, but we do: we want
@@ -36,7 +41,7 @@ init_tiles:
 	XOR A
 	LD [$FF00+$40],A
 
-	; Default palette: 0123
+	; Default palette: 3210
 	LD A, $E4
 	LD [$FF00+$48], A	; FIXME: handle OBJ palettes in gb.go
 
@@ -63,8 +68,19 @@ init_tiles:
 init_dma:
 	CALL install_dma_routine
 
-	; Init state and enable vblank interrupt.
-	LD HL, state_fade_logo
+start:
+	; Start our state machine of sorts: alternate wait with fade, then scroll
+	; sprite in.
+
+	; TODO: this begs for a macro.
+	LD A, LOW(state_fade_logo)
+	LD [NEXT_STATE_ADDR], A
+	LD A, HIGH(state_fade_logo)
+	LD [NEXT_STATE_ADDR+1], A
+	LD A, FADE_DELAY
+	LD [DELAY_ADDR], A
+
+	LD HL, state_wait_frames
 	LD A, $01
 	LD [$FF00+$FF], A
 	EI
@@ -99,25 +115,34 @@ joypad:
 ; Game states
 ;
 
+; Wait a given number of screen frames.
+; Initialize $D000 (DELAY_ADDR) with the number of vblanks to wait.
+; Initialize $D001-$D002 (NEXT_STATE_LADDR-NEXT_STATE_HADDR) with the address of 
+; the next state to execute.
+state_wait_frames:
+	PUSH HL
+	LD HL, DELAY_ADDR
+	DEC [HL]
+	POP HL	; State address
+	JR NZ, .end
+
+	; Delay's elapsed, load HL with next state to execute.
+	LD A, [NEXT_STATE_ADDR]
+	LD L, A
+	LD A, [NEXT_STATE_ADDR+1]
+	LD H, A
+.end:
+	RET
+
+
 ; Make Nintendo logo fade to white. Eebildz sprite is too big to scroll in
 ; without messing with it anyway.
 ; BIOS palette is $FC, but we only care about bits 2-3. They'll go from 3 to 0.
 state_fade_logo:
-	; Delay.
-	PUSH HL
-	LD HL, FADE_DELAY_ADDR
-	DEC [HL]
-	JR Z, .reset
-
-	; If delay's not elapsed, restore HL and break here.
-	POP HL
-	RET
-
-.reset:
-	; Reset if elapsed. Restore HL when done.
+	; Reset delay.
 	LD A, FADE_DELAY
-	LD [HL], A
-	POP HL
+	LD [DELAY_ADDR], A
+	LD HL, state_wait_frames
 
 	; Decrease palette entry.
 	LDH A, [$FF00+$47]
@@ -125,17 +150,43 @@ state_fade_logo:
 	LDH [$FF00+$47], A
 	JR NZ, .end
 
-	; Zero reached. Load next state.
-	LD HL, state_update_sprites
+	; Zero reached. Update next state to load after the next delay.
+	LD A, LOW(state_update_sprites)
+	LD [NEXT_STATE_ADDR], A
+	LD A, HIGH(state_update_sprites)
+	LD [NEXT_STATE_ADDR+1], A
+
 .end
 	RET
 
-
+LEAN_IN_DELAY EQU $04	; Sprite scroll speed
 state_update_sprites:
-	; TODO: actually update OAM shadow RAM
+	; Decrease X for each sprite until all is visible.
+	LD H, HIGH(WORK_OAM_ADDR)
+	LD A, $01	; Offset into current OAM entry (X position)
+.update_loop:
+	LD L, A
+	DEC [HL]
+	ADD A, $04	; Next entry
+	CP A, $A1
+	JR NZ, .update_loop
 
 	CALL $FF80	; DMA
-	; TODO: next state: LD HL, state_idle_wait
+	
+	; We're done when the last tile's X position is 'not quite visible'
+	LD A, 157
+	CP A, [HL]
+	JR NZ, .end
+
+	; We're... done for now.
+.hang:
+	JR .hang
+
+.end
+	; Reset delay.
+	LD A, LEAN_IN_DELAY
+	LD [DELAY_ADDR], A
+	LD HL, state_wait_frames
 	RET
 
 
@@ -173,46 +224,46 @@ sprites_data:
 	; XXX: base coordinates are top-left for now.
 	; Using $1A id offset to put our tiles right after the Nintendo logo.
 	;   Y    X    #    Palette
-	DB $10, $08, $00+$1A, $00	; #0  ( 8,16)
-	DB $10, $10, $01+$1A, $00	; #1  (16,16)
-	DB $10, $18, $02+$1A, $00	; #2  (24,16)
-	DB $10, $20, $03+$1A, $00	; #3  (32,16)
-	DB $10, $28, $04+$1A, $00	; #4  (40,16)
-	DB $18, $08, $05+$1A, $00	; #5  ( 8,24)
-	DB $18, $10, $06+$1A, $00	; #6  (16,24)
-	DB $18, $18, $07+$1A, $00	; #7  (24,24)
-	DB $18, $20, $08+$1A, $00	; #8  (32,24)
-	DB $18, $28, $09+$1A, $00	; #9  (40,24)
-	DB $20, $08, $0A+$1A, $00	; #10 ( 8,32)
-	DB $20, $10, $0B+$1A, $00	; #11 (16,32)
-	DB $20, $18, $0C+$1A, $00	; #12 (24,32)
-	DB $20, $20, $0D+$1A, $00	; #13 (32,32)
-	DB $20, $28, $0E+$1A, $00	; #14 (40,32)
-	DB $20, $30, $0F+$1A, $00	; #15 (48,32)
-	DB $28, $08, $10+$1A, $00	; #16 ( 8,40)
-	DB $28, $10, $11+$1A, $00	; #17 (16,40)
-	DB $28, $18, $12+$1A, $00	; #18 (24,40)
-	DB $28, $28, $13+$1A, $00	; #19 (40,40)
-	DB $30, $08, $14+$1A, $00	; #20 ( 8,48)
-	DB $30, $10, $15+$1A, $00	; #21 (16,48)
-	DB $30, $18, $16+$1A, $00	; #22 (24,48)
-	DB $30, $20, $17+$1A, $00	; #23 (32,48)
-	DB $30, $28, $18+$1A, $00	; #24 (40,48)
-	DB $38, $18, $19+$1A, $00	; #25 (24,56)
-	DB $38, $20, $1A+$1A, $00	; #26 (32,56)
-	DB $40, $18, $1B+$1A, $00	; #27 (24,64)
-	DB $40, $20, $1C+$1A, $00	; #28 (32,64)
-	DB $48, $18, $1D+$1A, $00	; #29 (24,72)
-	DB $48, $20, $1E+$1A, $00	; #30 (32,72)
-	DB $48, $28, $1F+$1A, $00	; #31 (40,72)
-	DB $50, $20, $20+$1A, $00	; #32 (32,80)
-	DB $50, $28, $21+$1A, $00	; #33 (40,80)
-	DB $50, $30, $22+$1A, $00	; #34 (48,80)
-	DB $58, $20, $23+$1A, $00	; #35 (32,88)
-	DB $58, $28, $24+$1A, $00	; #36 (40,88)
-	DB $60, $28, $25+$1A, $00	; #37 (40,96)
-	DB $68, $28, $26+$1A, $00	; #38 (40,104)
-	DB $70, $28, $27+$1A, $00	; #39 (40,112)
+	DB $10+$28, $08+$A0, $00+$1A, $00	; #0  ( 8,16)
+	DB $10+$28, $10+$A0, $01+$1A, $00	; #1  (16,16)
+	DB $10+$28, $18+$A0, $02+$1A, $00	; #2  (24,16)
+	DB $10+$28, $20+$A0, $03+$1A, $00	; #3  (32,16)
+	DB $10+$28, $28+$A0, $04+$1A, $00	; #4  (40,16)
+	DB $18+$28, $08+$A0, $05+$1A, $00	; #5  ( 8,24)
+	DB $18+$28, $10+$A0, $06+$1A, $00	; #6  (16,24)
+	DB $18+$28, $18+$A0, $07+$1A, $00	; #7  (24,24)
+	DB $18+$28, $20+$A0, $08+$1A, $00	; #8  (32,24)
+	DB $18+$28, $28+$A0, $09+$1A, $00	; #9  (40,24)
+	DB $20+$28, $08+$A0, $0A+$1A, $00	; #10 ( 8,32)
+	DB $20+$28, $10+$A0, $0B+$1A, $00	; #11 (16,32)
+	DB $20+$28, $18+$A0, $0C+$1A, $00	; #12 (24,32)
+	DB $20+$28, $20+$A0, $0D+$1A, $00	; #13 (32,32)
+	DB $20+$28, $28+$A0, $0E+$1A, $00	; #14 (40,32)
+	DB $20+$28, $30+$A0, $0F+$1A, $00	; #15 (48,32)
+	DB $28+$28, $08+$A0, $10+$1A, $00	; #16 ( 8,40)
+	DB $28+$28, $10+$A0, $11+$1A, $00	; #17 (16,40)
+	DB $28+$28, $18+$A0, $12+$1A, $00	; #18 (24,40)
+	DB $28+$28, $28+$A0, $13+$1A, $00	; #19 (40,40)
+	DB $30+$28, $08+$A0, $14+$1A, $00	; #20 ( 8,48)
+	DB $30+$28, $10+$A0, $15+$1A, $00	; #21 (16,48)
+	DB $30+$28, $18+$A0, $16+$1A, $00	; #22 (24,48)
+	DB $30+$28, $20+$A0, $17+$1A, $00	; #23 (32,48)
+	DB $30+$28, $28+$A0, $18+$1A, $00	; #24 (40,48)
+	DB $38+$28, $18+$A0, $19+$1A, $00	; #25 (24,56)
+	DB $38+$28, $20+$A0, $1A+$1A, $00	; #26 (32,56)
+	DB $40+$28, $18+$A0, $1B+$1A, $00	; #27 (24,64)
+	DB $40+$28, $20+$A0, $1C+$1A, $00	; #28 (32,64)
+	DB $48+$28, $18+$A0, $1D+$1A, $00	; #29 (24,72)
+	DB $48+$28, $20+$A0, $1E+$1A, $00	; #30 (32,72)
+	DB $48+$28, $28+$A0, $1F+$1A, $00	; #31 (40,72)
+	DB $50+$28, $20+$A0, $20+$1A, $00	; #32 (32,80)
+	DB $50+$28, $28+$A0, $21+$1A, $00	; #33 (40,80)
+	DB $50+$28, $30+$A0, $22+$1A, $00	; #34 (48,80)
+	DB $58+$28, $20+$A0, $23+$1A, $00	; #35 (32,88)
+	DB $58+$28, $28+$A0, $24+$1A, $00	; #36 (40,88)
+	DB $60+$28, $28+$A0, $25+$1A, $00	; #37 (40,96)
+	DB $68+$28, $28+$A0, $26+$1A, $00	; #38 (40,104)
+	DB $70+$28, $28+$A0, $27+$1A, $00	; #39 (40,112)
 .end
 
 ; Tile data generated with:
