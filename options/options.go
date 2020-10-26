fbeb9134dfbede142e8d17df91aa291d2429de56
kefen <lazy-stripes@tigris.fr>
2020-10-26 15:43:57 +0100

    External Options structure to easily pass to a GameBoy instance.

---
+++ options/options.go
@@ -0,0 +1,75 @@
+package options
+
+import (
+	"flag"
+	"fmt"
+)
+
+// Options structure grouping command line flags values.
+type Options struct {
+	CPUProfile   string // -cpuprofile <path>
+	Duration     uint   // -cycles <amount>
+	FastBoot     bool   // -fastboot
+	GIFPath      string // -gif <path>
+	NoSync       bool   // -nosync
+	ROMPath      string // -rom <path>
+	WaitKey      bool   // -waitkey
+	ZoomFactor   uint8  // -zoom <factor>
+	DebugLevel   string // -level <debug level>
+	DebugModules module // -debug <module>
+}
+
+// User-defined type to parse a list of module names for which debug output must be enabled.
+type module []string
+
+// String is the method to format the flag's value, part of the flag.Value interface.
+// The String method's output will be used in diagnostics.
+func (m *module) String() string {
+	return fmt.Sprint(*m)
+}
+
+// Set is the method to set the flag value, part of the flag.Value interface.
+// Set's argument is a string to be parsed to set the flag.
+// Flag can be specified multiple times.
+func (m *module) Set(value string) error {
+	*m = append(*m, value)
+	return nil
+}
+
+// Supported command-line options for the emulator.
+var cpuprofile = flag.String("cpuprofile", "", "Write cpu profile to file")
+var duration = flag.Uint("cycles", 0, "Stop after executing that many cycles")
+var debugModules module
+var debugLevel = flag.String("level", "warning", "Debug level (-level help for full list)")
+var fastBoot = flag.Bool("fastboot", false, "Bypass boot ROM execution")
+var gifPath = flag.String("gif", "", "Record gif file")
+var noSync = flag.Bool("nosync", false, "Do not sync to VBlank ever")
+var romPath = flag.String("rom", "", "ROM file to load")
+var waitKey = flag.Bool("waitkey", false, "Wait for keypress to start CPU (to help with screen captures)")
+var zoomFactor = flag.Int("zoom", 2, "Zoom factor (default is 2x)")
+
+// Initialize dynamic options.
+func init() {
+	flag.Var(&debugModules, "debug", "Turn on debug mode for the given module (-debug help for the full list)")
+}
+
+// Parse commend-line arguments and return their value in a struct the caller
+// can easily pass around.
+func Parse() *Options {
+	flag.Parse()
+
+	opt := Options{
+		CPUProfile:   *cpuprofile,
+		Duration:     *duration,
+		DebugModules: debugModules,
+		DebugLevel:   *debugLevel,
+		FastBoot:     *fastBoot,
+		GIFPath:      *gifPath,
+		NoSync:       *noSync,
+		ROMPath:      *romPath,
+		WaitKey:      *waitKey,
+		ZoomFactor:   uint8(*zoomFactor),
+	}
+
+	return &opt
+}
