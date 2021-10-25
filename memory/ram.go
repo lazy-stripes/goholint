a8180e4428026d47855d251a374cda08a1ae35c9
kefen <lazy-stripes@tigris.fr>
2021-10-25 22:57:57 +0200

    Remove memory.VRAM, it was never really needed.

--- memory/ram.go
+++ memory/ram.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 )
 
 // RAM as an array of R/W bytes at addresses starting from a given offset.
@@ -35,16 +34,6 @@ func (r *RAM) Write(addr uint16, value uint8) {
 // to wrap uint16 here.
 func (r *RAM) Contains(addr uint16) bool {
 	return addr >= r.Start && addr <= r.Start+uint16(len(r.Bytes))-1
-}
-
-// NewVRAM instantiates a slice of the given size to represent RAM, initialized
-// with random values.
-func NewVRAM(start, size uint16) *RAM {
-	vram := NewRAM(start, size)
-	for i := range vram.Bytes {
-		vram.Bytes[i] = uint8(rand.Intn(0xff))
-	}
-	return vram
 }
 
 // Load sets the current content of RAM from the given file, and stores the
