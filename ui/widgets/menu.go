71f57646683cb3318023a89c6432654c01e0df4b
kefen <lazy-stripes@tigris.fr>
2024-04-03 19:30:56 +0200

    Clean up Menu widget, only use one menuChoice type for entries.

--- ui/widgets/menu.go
+++ ui/widgets/menu.go
@@ -5,10 +5,10 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-// MenuChoice groups a choice string with the corresponding action.
-type MenuChoice struct {
-	Text   string
-	Action func()
+// menuChoice embedds a label widget and an action function on confirm.
+type menuChoice struct {
+	*Label
+	action func()
 }
 
 // Menu widget displaying a list of potential choices, each of which should map
@@ -16,37 +16,37 @@ type MenuChoice struct {
 type Menu struct {
 	*VerticalLayout
 
-	choices  []MenuChoice
 	selected int // Index of selected choice
+
+	labelSizeHint sdl.Rect   // Cached sizehint for adding choices.
+	labelProps    Properties // Cached properties for adding choices.
 }
 
-func NewMenu(sizeHint *sdl.Rect, choices []MenuChoice) *Menu {
+func NewMenu(sizeHint *sdl.Rect) *Menu {
 	props := DefaultProperties
 	props.HorizontalAlign = align.Center
 	props.VerticalAlign = align.Justified
-	layout := NewVerticalLayout(sizeHint, nil, props)
 
+	m := Menu{
+		VerticalLayout: NewVerticalLayout(sizeHint, nil, props),
+	}
+
+	// Cache labelSizeHint and label props for AddChoice
 	props.Margin = int32(DefaultProperties.Zoom * 8)
 	props.VerticalAlign = align.Middle
-	labelSizeHint := *sizeHint
-	labelSizeHint.H = int32(DefaultProperties.TitleFont.Height())
-	for i, c := range choices {
-		label := NewLabel(&labelSizeHint, c.Text, props)
+	m.labelProps = props
+	m.labelSizeHint = *sizeHint
+	m.labelSizeHint.H = int32(DefaultProperties.TitleFont.Height())
 
-		// Pre-select first item in list.
-		if i == 0 {
-			label.Background = label.BgColor
-			label.repaint()
-		}
+	return &m
+}
 
-		layout.Add(label)
-	}
-
-	return &Menu{
-		VerticalLayout: layout,
-		choices:        choices,
-		selected:       0,
-	}
+func (m *Menu) AddChoice(title string, action func()) {
+	m.Add(&menuChoice{
+		Label:  NewLabel(&m.labelSizeHint, title, m.labelProps),
+		action: action,
+	})
+	m.repaint()
 }
 
 func (m *Menu) ProcessEvent(e Event) bool {
@@ -77,8 +77,8 @@ func (m *Menu) ProcessEvent(e Event) bool {
 // current returns the selected character instance from the internal list of
 // children.
 // FIXME: lots of common code with input, I should write a SelectableLayout or something.
-func (m *Menu) current() *Label {
-	return m.children[m.selected].(*Label)
+func (m *Menu) current() *menuChoice {
+	return m.children[m.selected].(*menuChoice)
 }
 
 // XXX: Maybe wrap label in a common type that provides .highlight()
@@ -92,6 +92,15 @@ func (m *Menu) highlightCurrent(v bool) {
 	label.repaint()
 }
 
+func (m *Menu) Select(index uint) {
+	if index < uint(len(m.children)) {
+		m.highlightCurrent(false)
+		m.selected = int(index)
+		m.highlightCurrent(true)
+		m.repaint()
+	}
+}
+
 func (m *Menu) Up() {
 	m.highlightCurrent(false)
 	if m.selected > 0 {
@@ -103,12 +112,12 @@ func (m *Menu) Up() {
 
 func (m *Menu) Down() {
 	m.highlightCurrent(false)
-	if m.selected < len(m.choices)-1 {
+	if m.selected < len(m.children)-1 {
 		m.selected += 1
 	}
 	m.highlightCurrent(true)
 }
 
 func (m *Menu) Confirm() {
-	m.choices[m.selected].Action()
+	m.current().action()
 }
