83f2294bf988291948c7b013e8f502927e99d372
kefen <lazy-stripes@tigris.fr>
2024-08-21 23:20:15 +0200

    Make sure call Stack's current child's texture is always refreshed.

--- ui/widgets/stack.go
+++ ui/widgets/stack.go
@@ -11,12 +11,16 @@ type Stack struct {
 }
 
 func NewStack(sizeHint *sdl.Rect, children []Widget, props ...Properties) *Stack {
-	return &Stack{Group: NewGroup(sizeHint, children, props...)}
+	s := Stack{Group: NewGroup(sizeHint, children, props...)}
+	if len(children) > 0 {
+		s.Show(0)
+	}
+	return &s
 }
 
 // ProcessEvent calls ProcessEvent for the currently displayed widget if any.
 func (s *Stack) ProcessEvent(e Event) bool {
-	if len(s.children) >= 0 {
+	if len(s.children) > 0 {
 		return s.children[s.current].ProcessEvent(e)
 	}
 	return false
@@ -31,8 +35,16 @@ func (s *Stack) Add(w Widget) {
 	}
 }
 
+// Texture updates the internal texture with the currently shown child (if any)
+// then calls the base class.
+func (s *Stack) Texture() *sdl.Texture {
+	if len(s.children) > 0 {
+		s.texture = s.children[s.current].Texture()
+	}
+	return s.widget.Texture()
+}
+
 // Show sets the current index of the widget to be drawn and repaints the stack.
 func (s *Stack) Show(index uint) {
 	s.current = index
-	s.texture = s.children[s.current].Texture()
 }
