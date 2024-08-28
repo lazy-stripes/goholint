7ddef0acbdee9e9e4545d565cfa71a664a90fe29
kefen <lazy-stripes@tigris.fr>
2024-08-28 23:09:37 +0200

    Use OnVBlank to make sure to pause emulation on a full screen frame.

--- ui/actions.go
+++ ui/actions.go
@@ -7,6 +7,13 @@ import (
 // Action type for user interactions. TODO: see if we can use a single one for gameboy/ui.
 type Action func(eventType uint32)
 
+// sdlWrap returns the given function wrapped into an sdl.Do call, to be safely
+// used as a callback function. This helps keeping SDL entirely out of gameboy
+// code.
+func sdlWrap(f func()) func() {
+	return func() { sdl.Do(f) }
+}
+
 // Quit cleanly quits the program.
 func (u *UI) Quit(eventType uint32) {
 	if eventType != sdl.KEYDOWN {
@@ -23,7 +30,8 @@ func (u *UI) Home(eventType uint32) {
 	if u.paused {
 		u.Hide()
 	} else {
-		u.Show()
+		// Wait for full frame before pausing emulator.
+		u.screen.OnVBlank(sdlWrap(u.Show))
 	}
 }
 
