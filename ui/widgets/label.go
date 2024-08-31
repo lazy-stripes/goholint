28b065a6bf5bf36fa0c22c395cd59fd56176de69
kefen <lazy-stripes@tigris.fr>
2024-08-31 14:56:00 +0200

    Handle multiline text in Labels.

--- ui/widgets/label.go
+++ ui/widgets/label.go
@@ -1,41 +1,44 @@
 package widgets
 
 import (
+	"strings"
+
 	"github.com/veandco/go-sdl2/sdl"
-	"github.com/veandco/go-sdl2/ttf"
 )
 
 type Label struct {
 	*widget
 
-	font *ttf.Font
 	text string
 }
 
 // TODO: why couldn't these be methods of a global UI object abstracting the renderer?
 //       I'd just need to move all widgets back up to the ui package.
 // TODO: actual font size
-func NewLabel(sizeHint *sdl.Rect, text string, props ...Properties) *Label {
-	p := DefaultProperties
-	if len(props) > 0 {
-		p = props[0]
+func NewLabel(sizeHint *sdl.Rect, text string, p ...Properties) *Label {
+	props := DefaultProperties
+	if len(p) > 0 {
+		props = p[0]
 	}
 
-	return newLabel(sizeHint, text, p.Font, props...)
-}
-
-func newLabel(sizeHint *sdl.Rect, text string, font *ttf.Font, props ...Properties) *Label {
 	if sizeHint == noSizeHint {
-		// Query font size to create internal texture.
-		w, h, _ := font.SizeUTF8(text)
-		sizeHint = &sdl.Rect{
-			W: int32(w),
-			H: int32(h),
+		// Query font size to create our own size hint for the internal texture.
+		sizeHint = &sdl.Rect{}
+
+		// Allow Labels to have several lines. We'll have to compute the max
+		// width and total added height for the whole thing.
+		lines := strings.Split(text, "\n")
+		for _, line := range lines {
+			w, h, _ := props.Font.SizeUTF8(line)
+			if int32(w) > sizeHint.W {
+				sizeHint.W = int32(w)
+			}
+			sizeHint.H += int32(h)
 		}
+
 	}
 	l := &Label{
-		widget: new(sizeHint, props...),
-		font:   font,
+		widget: new(sizeHint, props),
 		text:   text,
 	}
 	l.repaint()
@@ -46,12 +49,14 @@ func (l *Label) repaint() {
 	l.clear()
 
 	// Instantiate text with an outline effect. There's probably an easier way.
-	l.font.SetOutline(DefaultProperties.Zoom)
-	outline, _ := l.font.RenderUTF8Blended(l.text, l.BgColor)
+	l.Font.SetOutline(DefaultProperties.Zoom)
+	// Render*Wrapped nicely handles newlines and will only wrap on them if
+	// called with wrap length 0.
+	outline, _ := l.Font.RenderUTF8BlendedWrapped(l.text, l.BgColor, 0)
 	defer outline.Free()
 
-	l.font.SetOutline(0)
-	text, _ := l.font.RenderUTF8Blended(l.text, l.FgColor)
+	l.Font.SetOutline(0)
+	text, _ := l.Font.RenderUTF8BlendedWrapped(l.text, l.FgColor, 0)
 	defer text.Free()
 
 	// Render text on top of outline, offset by outline width.
